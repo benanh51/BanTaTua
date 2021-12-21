ff79343079edc61eb3b4034e47febe0892a35b2d
lamtieutu <81681595+lamtieutu@users.noreply.github.com>
2021-12-21 14:24:07 +0700

    .

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+
+    <title>Document</title>
+</head>
+<body>
 <div class="w-screen h-screen overflow-hidden ">
   <nav class="flex items-center justify-between flex-wrap bg-red-500 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
@@ -25,3 +37,5 @@
     </div>
   </nav>
 </div>
+</body>
+</html>
