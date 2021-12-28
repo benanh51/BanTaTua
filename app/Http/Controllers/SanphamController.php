c804dfcb3349a815ebd5201395d47041118a4bc2
lamtieutu <81681595+lamtieutu@users.noreply.github.com>
2021-12-28 15:39:08 +0700

    .

--- app/Http/Controllers/SanphamController.php
+++ app/Http/Controllers/SanphamController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class SanphamController extends Controller
 {
+    /*public function index()
+    {
+        $ds = $this -> danhsachphong();
+        return view('pages.phongban') -> with('ds', $ds);
+    }
+    //lay thong tin tu view luu vao csdl
+    public function save(Request $request)
+    {
+        $pb = DB::table('phongban')->insert(['tenphong'=>$request->tenphong,'viettat'=>$request->viettat,
+                                                'ghichu'=>$request->ghichu]);
+        $error = $pb? 'Insert thành công' : 'Có lỗi xảy ra';
+        $ds = $this->danhsachphong();
+        return view('pages.phongban')->with(['error'=>$error,'ds'=>$ds]);
+    }
+
+    function danhsachphong()
+    {
+        $ds = DB::table('phongban')->get();
+        return $ds;
+    }*/
     
 }
